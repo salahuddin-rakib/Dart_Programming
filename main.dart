ec22d468ab548de060792eef5f9a5b805dbb5ae9
Rakib <rakib@misfit.tech>
2021-09-11 17:03:38 +0600

    Using all variable types

--- main.dart
+++ main.dart
@@ -1,9 +1,17 @@
 void main() {
-  String character_name = "Oscar";
-  String characterAge = "18";
+  // String character_name = "Oscar";
+  // String characterAge = "18";
 
-  print("There was a man called ${character_name}.");
-  print("He was ${characterAge} years old.");
-  print("He really liked the name ${character_name}.");
-  print("But he didn't like being ${characterAge}.");
+  // print("There was a man called ${character_name}.");
+  // print("He was ${characterAge} years old.");
+  // print("He really liked the name ${character_name}.");
+  // print("But he didn't like being ${characterAge}.");
+
+  String fullName = "Salahuddin Rakib  :)";
+  int age = 26;
+  double gpa = 3.5;
+  bool is_true = true;
+
+  print(
+      "Hello ${fullName}, Are you ${age} years old and Is your GPA ${gpa}? Answer using ${is_true} of false");
 }
