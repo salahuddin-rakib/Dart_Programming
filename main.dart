a961c18819fcfa784250636529745469690b63fe
Rakib <rakib@misfit.tech>
2021-09-11 18:03:59 +0600

    contain and index function of string.

--- main.dart
+++ main.dart
@@ -1,5 +1,12 @@
 void main() {
   String gretings = "Hello";
   print(gretings.length); // Output: 5
-  print(gretings[3]);
+  print(gretings[3]); // Output: l
+
+  print(gretings.toLowerCase()); // Output: hello
+  print(gretings.toUpperCase()); // Output: HELLO
+
+  // If the character is inside of this character:
+  print(gretings.indexOf('o')); // Output: 4
+  print(gretings.contains('o')); // Output: true
 }
