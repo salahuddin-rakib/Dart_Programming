dd681e085eaef50b4a7eb74cc9cfbfea3ead51b3
Rakib <rakib@misfit.tech>
2021-09-11 18:39:43 +0600

    String concatination and Interpolation.

--- main.dart
+++ main.dart
@@ -9,4 +9,13 @@ void main() {
   // If the character is inside of this character:
   print(gretings.indexOf('o')); // Output: 4
   print(gretings.contains('o')); // Output: true
+
+  // String concatination and Interpolation
+  String gretings2 = "World";
+
+  // Concatination:
+  print(gretings + " " + gretings2); // Output: Hello World
+
+  // Interpolation:
+  print("The gretings is: ${gretings} ${gretings2}"); // Output: Hello World
 }
