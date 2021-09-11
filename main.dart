aeb962618bc9b4d5b06c93225d3f9be549944c64
Rakib <rakib@misfit.tech>
2021-09-11 16:52:29 +0600

    Saving characterName and Age in String Variable.

--- main.dart
+++ main.dart
@@ -1,6 +1,9 @@
 void main() {
-  print("There was a man called Stanley.");
-  print("He was 70 years old.");
-  print("He really liked the name Stanley.");
-  print("But he didn't like being 70.");
+  String character_name = "Oscar";
+  String characterAge = "18";
+
+  print("There was a man called ${character_name}.");
+  print("He was ${characterAge} years old.");
+  print("He really liked the name ${character_name}.");
+  print("But he didn't like being ${characterAge}.");
 }
